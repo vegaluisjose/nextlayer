9501a7442a07fcdfc3e7e3e82e823544cd6d004d
Luis Vega <vegaluisjose@gmail.com>
2020-07-01 12:52:21 -0700

    load rdi library

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use std::ffi::CString;
-use std::os::raw::{c_void, c_char};
 use libloading::{Library, Symbol};
+use std::ffi::CString;
+use std::os::raw::{c_char, c_void};
+use std::path::Path;
 
 #[repr(C)]
 pub struct XsimInfo {
@@ -11,17 +12,29 @@ pub struct XsimInfo {
 type XsimOpen = fn(*const XsimInfo) -> *mut c_void;
 
 pub struct Xsim {
-    tb_handle: *mut c_void,
+    sim_lib: Library,
+    design_handle: *mut c_void,
 }
 
 impl Xsim {
     pub fn new() -> Xsim {
-        let tb_lib = Library::new("xsim/xsim.dir/work.testbench/xsimk.so").expect("Error: could not load testbench lib");
+        let design_handle = Library::new("xsim/xsim.dir/work.testbench/xsimk.so")
+            .expect("Error: could not load design lib");
         let path = CString::new("").expect("Error: specifying path"); // empty for now
+        let lnx64_path = Path::new("/tools/Xilinx/Vivado/2020.1/lib/lnx64.o");
         unsafe {
-            let xsim_info = XsimInfo{ log_file: path.as_ptr(), wdb_file: path.as_ptr() };
-            let xsim_open: Symbol<XsimOpen> = tb_lib.get(b"xsi_open").expect("Error: could not find xsi_open");
-            Xsim { tb_handle: xsim_open(&xsim_info) }
+            let xsim_info = XsimInfo {
+                log_file: path.as_ptr(),
+                wdb_file: path.as_ptr(),
+            };
+            let xsim_open: Symbol<XsimOpen> = design_handle
+                .get(b"xsi_open")
+                .expect("Error: could not find xsi_open");
+            Xsim {
+                sim_lib: Library::new(lnx64_path.join("librdi_simulator_kernel.so"))
+                    .expect("Error: could not load sim lib"),
+                design_handle: xsim_open(&xsim_info),
+            }
         }
     }
 }
