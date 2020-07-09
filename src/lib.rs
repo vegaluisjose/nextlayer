d8f9c012a18a16525aa0cc87ad06cece779a0997
Luis Vega <vegaluisjose@gmail.com>
2020-07-09 05:50:44 +0000

    add adder example

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,37 @@ use crate::backend::xsim::Xsim;
 pub extern fn run_xsim() {
     println!("Running...");
     let mut sim = Xsim::new();
+    // reset for 10 cycles
+    sim.poke("mask", 0);
     for _ in 0..10 {
         sim.poke("reset", 1);
-        sim.poke("clock", 0);
-        sim.eval();
         sim.poke("clock", 1);
+        sim.eval();
+        sim.poke("clock", 0);
         sim.eval();
     }
     sim.poke("reset", 0);
-    for _ in 0..10 {
-        sim.poke("clock", 0);
-        sim.eval();
+    // write a
+    sim.poke("opcode", 1);
+    sim.poke("id", 0);
+    sim.poke("in", 3);
+    sim.eval();
+    // write b
+    sim.poke("opcode", 1);
+    sim.poke("id", 1);
+    sim.poke("in", 4);
+    sim.eval();
+    // run for 1 cycle
+    for _ in 0..1 {
         sim.poke("clock", 1);
         sim.eval();
-        println!("count:{}", sim.peek("count"));
+        sim.poke("clock", 0);
+        sim.eval();
     }
+    // read y
+    sim.poke("opcode", 2);
+    sim.poke("id", 2);
+    sim.eval();
+    println!("adder result:{}", sim.peek("out"));
     println!("Finishing...");
 }
--- xsim/testbench.v
+++ xsim/testbench.v
@@ -1,38 +1,117 @@
+// opcode
+// 0 - nop
+// 1 - write reg
+// 2 - read reg
+// 3 - write mem
+// 4 - read mem
+
+// all of this will be generated
+
 module testbench (
-   input         clock,
-   input         reset,
-   output [31:0] count,
-   input         update,
-   input [31:0]  value
+    input  logic        clock,
+    input  logic        reset,
+    input  logic [31:0] opcode,
+    input  logic [31:0] id,
+    input  logic [31:0] mask,
+    input  logic [31:0] in,
+    output logic [31:0] out
 );
 
-    function void modify;
+    function void write_reg_a;
         input logic [31:0] value;
+        input logic [31:0] mask;
         logic [31:0] tmp;
         begin
-            tmp = 0;
-            tmp = testbench.cnt;
-            testbench.cnt = value;
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.a;
+            tmp[mask*32+:32] = value;
+            testbench.dut.a = tmp[0+:8];
         end
     endfunction
 
-    logic [31:0] cnt;
-
-    always_ff @(posedge clock) begin
-        if (reset) begin
-            cnt <= 32'd0;
+    function int read_reg_a;
+        input int mask;
+        logic [32-1:0] tmp;
+        begin
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.a;
+            return tmp[mask*32+:32];
         end
-        else begin
-            cnt <= cnt + 1'b1;
-        end
-    end
+    endfunction
 
-    assign count = cnt;
+    function void write_reg_b;
+        input logic [31:0] value;
+        input logic [31:0] mask;
+        logic [31:0] tmp;
+        begin
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.b;
+            tmp[mask*32+:32] = value;
+            testbench.dut.b = tmp[0+:8];
+        end
+    endfunction
+
+    function int read_reg_b;
+        input int mask;
+        logic [32-1:0] tmp;
+        begin
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.b;
+            return tmp[mask*32+:32];
+        end
+    endfunction
+
+    function void write_reg_y;
+        input logic [31:0] value;
+        input logic [31:0] mask;
+        logic [31:0] tmp;
+        begin
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.y;
+            tmp[mask*32+:32] = value;
+            testbench.dut.b = tmp[0+:8];
+        end
+    endfunction
+
+    function int read_reg_y;
+        input int mask;
+        logic [32-1:0] tmp;
+        begin
+            assert (mask < 1) else $error("mask out of bounds");
+            tmp[0+:32] = 0;
+            tmp[0+:8] = testbench.dut.y;
+            return tmp[mask*32+:32];
+        end
+    endfunction
 
     always_comb begin
-        if (update) begin
-            modify(value);
-        end
+        case(opcode)
+            32'd0 : out = 32'hdeadbeef;
+            32'd1 : begin
+                case(id)
+                    32'd0 : write_reg_a(in, mask);
+                    32'd1 : write_reg_b(in, mask);
+                    32'd2 : write_reg_y(in, mask);
+                    default : $error("invalid id");
+                endcase
+            end
+            32'd2 : begin
+                case(id)
+                    32'd0 : out = read_reg_a(mask);
+                    32'd1 : out = read_reg_b(mask);
+                    32'd2 : out = read_reg_y(mask);
+                    default : $error("invalid id");
+                endcase
+            end
+            default : $error("invalid opcode");
+        endcase
     end
+
+    adder dut (.clock(clock), .reset(reset));
 
 endmodule
