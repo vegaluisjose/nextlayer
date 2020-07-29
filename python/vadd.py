cce9def686788d1f4271795cf3809a67b0636534
Luis Vega <vegaluisjose@gmail.com>
2020-07-29 22:37:35 +0000

    update crate info

--- python/vadd.py
+++ python/vadd.py
@@ -2,7 +2,7 @@ from os import environ, getenv, path
 from ctypes import CDLL, c_int
 
 cur_dir = path.dirname(path.realpath(__file__))
-sim_lib = path.join(cur_dir, "../target/release/liblastlayer.so")
+sim_lib = path.join(cur_dir, "../target/release/libnextlayer.so")
 
 lib = CDLL(sim_lib)
 
